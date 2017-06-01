a253290250960d275693792d84552d86f338cc13
Andrew Patterson <andrew.patterson.3001@gmail.com>
2017-06-01 16:48:28 -0700

    Add logic to handle sorting of columns when clicking on table headers.

--- app/assets/javascripts/components/StudentsTable.jsx
+++ app/assets/javascripts/components/StudentsTable.jsx
@@ -1,14 +1,48 @@
 var StudentsTable = React.createClass({
     getInitialState: function() {
-        return { students: this.props.data };
+        return { students: this.props.data,
+                 sortByColumn: 'name',
+                 sortAlpha: true
+                };
     },
 
     getDefaultProps: function() {
-        return { students: [] };
+        return { students: [],
+                 sortByColumn: 'name',
+                 sortAlpha: true
+                };
     },
 
     refreshTable: function(data) {
-        this.replaceState({students: data})
+        this.setState({ students: data,
+                        sortByColumn: 'name',
+                        sortAlpha: true
+                        })
+    },
+
+    handleSort: function(column) {
+        if (column === this.state.sortByColumn) {
+            this.setState({ sortAlpha: !this.state.sortAlpha })
+        } else {
+            if (column === 'name') {
+                this.setState({ sortByColumn: 'name',
+                                    sortAlpha: !this.state.sortAlpha })
+            } else {
+                this.setState({ sortByColumn: 'favoritemovie',
+                                    sortAlpha: !this.state.sortAlpha })
+            }
+        }
+    },
+
+    orderColumns: function(property) {
+        var sortOrder = 1;
+        if(this.state.sortAlpha === false) {
+            sortOrder = -1;
+        }
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
     },
 
     render: function() {
@@ -17,14 +51,14 @@ var StudentsTable = React.createClass({
                     <table className="table">
                         <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>FavoriteMovie</th>
+                            <th onClick={() => this.handleSort('name')}>Name</th>
+                            <th onClick={() => this.handleSort('movie')}>Favorite Movie</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {this.state.students.map(function(student) {
-                            return <StudentRow key={student.id} student={student} />
-                        }.bind(this))}
+                            {this.state.students.sort(this.orderColumns(this.state.sortByColumn)).map(function(student) {
+                                return <StudentRow key={student.id} student={student} />
+                            }.bind(this))}
                         </tbody>
                     </table>
                 </div>);
