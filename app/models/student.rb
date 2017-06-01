38429f5f0a120568702b388a8df44bedc484d034
Andrew Patterson <andrew.patterson.3001@gmail.com>
2017-05-31 23:07:29 -0700

    use an alias method for movie instead of attr_accessor because it was overwriting the built in rails setter and preventing creation of seed file.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,4 @@
 class Student < ActiveRecord::Base
-  attr_accessor :favoritemovie
-  alias_attribute :movie, :favoritemovie
 
   #TODO: ANDREW add validations
 
@@ -10,5 +8,9 @@ class Student < ActiveRecord::Base
 
   def name
     self.firstname + (Student.repeated_firstnames.include?(self.firstname) ? ' ' + self.lastname[0] + '.' : '')
+  end
+
+  def movie
+    self.favoritemovie
   end
 end
