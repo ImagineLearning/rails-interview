d64678f8b366730c6823a9ca3776433dfef93b58
Andrew Patterson <andrew.patterson.3001@gmail.com>
2017-06-01 09:52:59 -0700

    Add nested route to js action within students controller and corresponding view file. Add react file structure.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,4 +18,25 @@ class StudentsController < ApplicationController
       end
     end
   end
+
+
+  def js
+    if params[:movie]
+      students_table = Student.arel_table
+      @students = Student.where(students_table[:favoritemovie].matches("%#{params[:movie]}%"))
+      if request.headers["Content-Type"] == "application/json"
+        render json: @students
+      else
+        @students
+      end
+    else
+      #TODO: ANDREW use pagination to avoid pulling all students at once
+      @students = Student.all
+      if request.headers["Content-Type"] == "application/json"
+        render json: @students
+      else
+        @students
+      end
+    end
+  end
 end
