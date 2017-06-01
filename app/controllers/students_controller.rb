ce626451f553e0837da4bc4432f31131d0ca98e3
Andrew Patterson <andrew.patterson.3001@gmail.com>
2017-06-01 09:04:09 -0700

    /students returns JSON when given a request with headers that specify Content-Type as application/json

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
   def index
-    #TODO: ANDREW use pagination to avoid pulling all students at once
     if params[:movie]
       students_table = Student.arel_table
       @students = Student.where(students_table[:favoritemovie].matches("%#{params[:movie]}%"))
+      if request.headers["Content-Type"] == "application/json"
+        render json: @students
+      else
+        @students
+      end
     else
+      #TODO: ANDREW use pagination to avoid pulling all students at once
       @students = Student.all
+      if request.headers["Content-Type"] == "application/json"
+        render json: @students
+      else
+        @students
+      end
     end
   end
 end
