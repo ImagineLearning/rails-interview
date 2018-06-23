8ba44ba92a79390a0d0cdac8cf8ab0ba5998b88a
partydrone <partydrone@me.com>
2018-06-23 17:42:25 -0600

    Move student list item component into student-list folder

--- client/src/app/students/students.module.ts
+++ client/src/app/students/students.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentListComponent } from './student-list/student-list.component';
-import { StudentListItemComponent } from './student-list-item/student-list-item.component';
+import { StudentListItemComponent } from './student-list/student-list-item.component';
 import { StudentService } from './student.service';
 
 @NgModule({
