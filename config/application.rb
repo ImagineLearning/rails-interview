d9f5c5fc2988832af34df4c8007d9e456aae8cab
partydrone <partydrone@me.com>
2018-06-22 21:56:30 -0600

    Configure logging to use STDOUT
    This allows you to follow the log output in the Docker container using `docker-compose logs -f`.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,18 @@ module Interview
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Set up logging to be the same in all environments but control the level
+    # through an environment variable.
+    config.log_level = ENV['LOG_LEVEL']
+
+    # Log to STDOUT because Docker expects all processes to log here. You could
+    # then redirect logs to a third party service on your own such as systemd,
+    # or a third party host such as Loggly, etc..
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
