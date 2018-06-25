f6b624c9f98bf94deaca25ebb2f2292ca10a976a
partydrone <partydrone@me.com>
2018-06-25 16:14:54 -0600

    Complete final task
    Implemented the "just checking" stuff at the very end of the README.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,19 @@
 class StudentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :just_checking
+
   def index
     @students = Student.search(params[:movie])
   end
 
   def js
 
+  end
+
+  def just_checking
+    if params[:valid] == 'true'
+      render text: 'BAR', status: :accepted
+    else
+      raise 'Kablooey! Sorry, this is not a valid request.'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     get 'js', on: :collection
   end
 
+  post 'foo', to: 'students#just_checking'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
